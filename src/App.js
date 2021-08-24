62ed2ddc53b78abfd6516148b8574e59a93e9b2f
Saksham <2020UCP1801@mnit.ac.in>
2021-08-24 14:48:27 +0530

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
