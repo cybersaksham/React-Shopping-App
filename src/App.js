43211cdc36209135a09a6452f646d59c8bc27792
Saksham <2020UCP1801@mnit.ac.in>
2021-08-24 15:45:25 +0530

    items defined

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { React, useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  const [items, setItems] = useState(
+    JSON.parse(sessionStorage.getItem("items")) || [
+      {
+        title: "First Item",
+        desc: "This is the first item",
+        cart: false,
+      },
+      {
+        title: "Second Item",
+        desc: "This is the second item",
+        cart: false,
+      },
+      {
+        title: "Third Item",
+        desc: "This is the third item",
+        cart: false,
+      },
+      {
+        title: "Fourth Item",
+        desc: "This is the fourth item",
+        cart: false,
+      },
+      {
+        title: "Fifth Item",
+        desc: "This is the fifth item",
+        cart: false,
+      },
+    ]
+  );
+
   return (
     <>
       <Navbar />
