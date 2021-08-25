532eed14ac918be0cb77dc437e97c3bedc1aecc3
Saksham <2020UCP1801@mnit.ac.in>
2021-08-25 12:43:26 +0530

    cart displayed

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Cart() {
-  return <div></div>;
+export default function Cart({ items }) {
+  const initCart = () => {
+    let cart__ = JSON.parse(sessionStorage.getItem("cart")) || [];
+    let fin_cart__ = [];
+    items.forEach((item) => {
+      if (cart__.indexOf(item.id) !== -1) fin_cart__.push(item);
+    });
+    return fin_cart__;
+  };
+
+  const [cart, setCart] = useState(initCart());
+
+  return (
+    <div className="container my-3">
+      <ol className="list-group list-group">
+        {cart.map((item) => {
+          return (
+            <li
+              key={item.id}
+              className="list-group-item d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">{item.title}</div>
+                {item.desc}
+              </div>
+              <div className="ms-2">
+                <span className="badge bg-primary rounded-pill">
+                  $ {item.price}
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
 }
